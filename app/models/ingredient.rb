30ff5da09f8f2df69626c15ccf773cb3d62efe02
Ricardo Piras <ricpiras@gmail.com>
2018-11-08 17:18:38 +0900

    Stylized the pages.

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,7 +1,6 @@
 class Ingredient < ApplicationRecord
   has_many :doses
   has_many :cocktails, through: :doses
-  #before_destroy :check_for_cocktails
 
   validates :name, uniqueness: true, presence: true
 
