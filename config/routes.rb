5a42937665c65cd10d6b567614e67fac4adcc7a4
Ricardo Piras <ricpiras@gmail.com>
2018-12-18 19:15:58 +0900

    New routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  root to: "cocktails#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :cocktails, only: [:index, :show, :new, :create, :destroy] do
-    resources :doses, only: [:new, :create]
+  resources :cocktails, except: [:destroy, :edit, :update] do
+    resources :doses, only: :create
+    resources :reviews, only: :create
   end
   resources :doses, only: [:destroy]
-
+  root 'cocktails#index'
 end
