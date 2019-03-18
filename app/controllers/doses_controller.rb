609dda6908cd159565010312945bb971d4a22c56
Ricardo Piras <ricpiras@gmail.com>
2019-03-18 12:10:34 +0900

    Doses controller improved.

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -2,38 +2,28 @@ class DosesController < ApplicationController
   before_action :set_cocktail, only: :create
 
   def new
-    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new
-    @ingredients = Ingredient.all
+    @cocktail = Cocktail.find(params[:cocktail_id])
   end
 
   def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
     @dose.cocktail = @cocktail
     if @dose.save
       redirect_to cocktail_path(@cocktail)
     else
-      @review = Review.new
-      render 'cocktails/show'
+      render :new
     end
   end
 
   def destroy
     @dose = Dose.find(params[:id])
-    cocktail = @dose.cocktail
     @dose.destroy
-    redirect_to cocktail_path(cocktail)
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
   private
-
-  def set_cocktail
-    @cocktail = Cocktail.find(params[:cocktail_id])
-  end
-
-  def set_dose
-    @dose = Dose.find(params[:id])
-  end
 
   def dose_params
     params.require(:dose).permit(:description, :ingredient_id)
